d2208c083ff475e02853f5aa09c9a4c1440e0c15
ManishRana0725 <manishbadm0725@gamil.com>
2024-11-17 17:30:12 +0530

    created all the CRUD opration and styled all the pages

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
-// const methodOverride = require("method-override");
-
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("views" , path.join(__dirname , "views"));
 app.set("view engine" , "ejs");
 app.use(express.urlencoded({extended: true}));
-// app.use(methodOverride("_method"));
+app.use(methodOverride("_method"));
+app.engine("ejs" , ejsMate);
+app.use(express.static(path.join(__dirname , "/public")));
 
 main().then((   )=>{
     console.log("connected to DB  ");
@@ -33,13 +35,50 @@ app.get("/listings" , async(req , res)=>{
     res.render("listings/index.ejs" , {allListings});
 });
 
+  
+//new route 
+app.get("/listings/new" , (req , res)=>{
+    res.render("listings/new.ejs");
+})
+
 // show by id  route
 app.get("/listings/:id", async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
 });
-  
+
+// create route
+app.post("/listings", async(req , res)=>{
+    const newlisting = new Listing(req.body.listing);
+    await newlisting.save();
+    res.redirect("/listings");
+})
+
+// edit
+app.get("/listings/:id/edit" , async(req , res)=>{
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs" , {listing});
+})
+
+// update route 
+app.put("/listings/:id" , async(req , res)=>{
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id , {...req.body.listing});
+    res.redirect("/listings");
+
+
+   
+});
+
+// delete route
+app.delete("/listings/:id" , async(req, res)=>{
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+})
+
 
 // app.get("/testListing" , async(req , res)=>{
 //     let sampleListing = new Listing({
